a8bf86b8992b8c0673f6272fc6bfeb06e4356cc9
Lachezar Kutsarov <lkutsarov@gmail.com>
2021-11-06 01:00:52 +0100

    Update SolutionIterative.js

--- SolutionIterative.js
+++ SolutionIterative.js
@@ -39,9 +39,7 @@ function searchForLeftBoundary(nums, left, right, target) {
 
         if (nums[mid] > target || nums[mid] === target) {
             right = mid - 1;
-        }
-
-        if (nums[mid] < target) {
+        } else {
             left = mid + 1;
         }
     }
@@ -65,12 +63,10 @@ function searchForRightBoundary(nums, left, right, target) {
             return mid;
         }
 
-        if (nums[mid] > target) {
-            right = mid - 1;
-        }
-
         if (nums[mid] < target || nums[mid] === target) {
             left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
     return -1;
